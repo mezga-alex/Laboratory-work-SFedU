c308b029e30863861f1320fb5b73056fa5254b9c
Mezga Alexandr <mezga.studio@gmail.com>
2019-10-20 23:38:41 +0300

    bit array c#

---
+++ C#/Task_4_Mezga/Task_4_Mezga/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bit_array
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArraySimple bit_array = new BitArraySimple(19);
+            //bit_array.set_byte(, 1);
+            //Console.WriteLine(bit_array.get_byte(0));
+
+            //bool f = 0;
+            bit_array.set_bit(1, 1);
+            bit_array.set_bit(1, 5);
+            bit_array.set_bit(1, 8);
+            bit_array.set_bit(1, 9);
+            bit_array.set_bit(1, 12);
+            bit_array.set_bit(1, 16);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Task_4_Mezga/Task_4_Mezga/bit_array.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace bit_array
+{
+    public class BitArraySimple
+    {
+        private byte[] arr;
+        public BitArraySimple(int n)
+        {
+            int size = n / 8;
+            if (n % 8 != 0)
+            {
+                size++;
+            } 
+
+            //Console.WriteLine(size);
+            this.arr = new byte[size];
+        }
+
+        public void set_byte(byte cur_byte, int pos)
+        {
+            this.arr[pos] = cur_byte;
+        }
+
+        public int get_byte(int pos)
+        {
+            return this.arr[pos];
+        }
+
+        public void set_bit(byte val, int pos)
+        {
+            if (pos < 1 || pos > this.arr.Length * 8)
+            {
+                Console.WriteLine("Ti daun");
+                return;
+            }
+            
+            if (val > 1)
+            {
+                Console.WriteLine("Only 0 and 1 available");
+                return;
+            }
+            Console.WriteLine($"Set {val} on position {pos}");
+            pos--;
+            int byte_pos = pos / 8;
+            int bit_pos = pos % 8;
+
+            bit_pos = 7 - bit_pos;
+            val <<= bit_pos;
+            //Console.WriteLine($"Shifted byte: {Convert.ToString(val, toBase: 2)}");
+            this.arr[byte_pos] |= val;
+            
+            Console.WriteLine($"This is {byte_pos} byte with {bit_pos} bit pos(0..7, start from right)");
+            Console.WriteLine($"Mask: {Convert.ToString(val, toBase: 2)}");
+            Console.WriteLine($"Shifted byte: {Convert.ToString(this.arr[byte_pos], toBase: 2)}");
+            Console.WriteLine();
+        }
+    }
+}
